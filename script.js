e8d24307bb98fa75699c7a50d7801570268334a8
DeeBest <simphiwedladla8@gmail.com>
2023-10-24 07:02:22 +0200

    added a function to change text content of each box that is clicked

--- script.js
+++ script.js
@@ -5,18 +5,22 @@ function game() {
         startGame.style.display = 'block';
         startGameBtn.style.display = 'none';
     });
+    let currentPlayer = 'X';
+    const gameBoard = document.querySelectorAll('#gameBoard .square');
+    const gameBoardArray = Array.from(gameBoard);
+    gameBoardArray.forEach((item, index) => {
+        item.addEventListener('click', () => {
+            console.log(`clicked item ${index}`)
+            item.textContent = currentPlayer;
+            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        });
+    });
+    
     const restartBtn = document.getElementById('restart-btn');
     restartBtn.addEventListener('click', () => {
         startGame.style.display = 'none';
         startGameBtn.style.display = 'block';
         console.log(' restart button clicked');
     });
-    const gameBoard = document.querySelectorAll('#gameBoard .square');
-    const gameBoardArray = Array.from(gameBoard);
-    gameBoardArray.forEach((item, index) => {
-        item.addEventListener('click', () => {
-            console.log(`clicked item ${index}`)
-        });
-    });
-} 
+}
 game();
