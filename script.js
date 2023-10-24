d598917984c3ab7f06a37233e6fa353c48d369a3
DeeBest <simphiwedladla8@gmail.com>
2023-10-24 06:39:01 +0200

    i have added the start and restart buttons functions and created the gameboard array

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-const game = document.getElementById('game');
-const startGameBtn = document.getElementById('start-game-btn');
-      startGameBtn.addEventListener('click', () => {
-              game.style.display = 'block';
-              startGameBtn.style.display = 'none';
-      });
-
-const restartBtn = document.getElementById('restart-btn');
-      restartBtn.addEventListener('click', () => {
-            game.style.display = 'none';
-            startGameBtn.style.display = 'block';
-      });
+function game() {
+    const startGame = document.getElementById('game');
+    const startGameBtn = document.getElementById('start-game-btn');
+    startGameBtn.addEventListener('click', () => {
+        startGame.style.display = 'block';
+        startGameBtn.style.display = 'none';
+    });
+    const restartBtn = document.getElementById('restart-btn');
+    restartBtn.addEventListener('click', () => {
+        startGame.style.display = 'none';
+        startGameBtn.style.display = 'block';
+        console.log(' restart button clicked');
+    });
+    const gameBoard = document.querySelectorAll('#gameBoard .square');
+    const gameBoardArray = Array.from(gameBoard);
+    gameBoardArray.forEach((item, index) => {
+        item.addEventListener('click', () => {
+            console.log(`clicked item ${index}`)
+        });
+    });
+} 
+game();
