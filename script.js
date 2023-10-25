df7573f0e940d4408ca959b80d0fdb56dc7105e0
DeeBest <simphiwedladla8@gmail.com>
2023-10-25 05:38:14 +0200

    added input fields to get players names instead of getting the through prompt.

--- script.js
+++ script.js
@@ -1,87 +1,6 @@
-// function game() {
-//     const startGame = document.getElementById('game');
-//     const startGameBtn = document.getElementById('start-game-btn');
-//     const winnerDisplay = document.getElementById('winner-display');
-//     startGameBtn.addEventListener('click', () => {
-//         startGame.style.display = 'block';
-//         startGameBtn.style.display = 'none';
-//     });
-//     let currentPlayer = 'X';
-//     let gameWon = false;
-
-//     // An array of all possible winning combinations for the game board
-//     const winningCombinations = [
-//         [0, 1, 2],
-//         [3, 4, 5],
-//         [6, 7, 8],
-//         [0, 3, 6],
-//         [1, 4, 7],
-//         [2, 5, 8],
-//         [0, 4, 8],
-//         [2, 4, 6],
-//     ];
-
-//     // A function that checks if a player has won the game
-//     function checkWinningCombination(player) {
-//         return winningCombinations.some(combination => {
-//             return combination.every(index => {
-//                 return gameBoardArray[index].textContent === player;
-//             });
-//         });
-//     }
-//     function checkTieCondition() {
-//         return gameBoardArray.every(item => {
-//             return item.classList.contains('clicked');
-//         });
-//     }
-
-//     const gameBoard = document.querySelectorAll('#gameBoard .square');
-//     const gameBoardArray = Array.from(gameBoard);
-//     //checking clicked boxes
-//     gameBoardArray.forEach((item) => {
-//         item.addEventListener('click', () => {
-//             if (!item.classList.contains('clicked') && !gameWon) {
-//                 item.textContent = currentPlayer;
-//                 item.classList.add('clicked');
-//                 if (checkWinningCombination(currentPlayer)) {
-//                     winnerDisplay.textContent = `${currentPlayer} wins! Restart the game to play again.`;
-//                     gameWon = true;
-//                 } else if (checkTieCondition()) {
-//                     winnerDisplay.textContent = `Tie game! Restart the game.`;
-//                 } else {
-
-//                     currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-//                 };
-//             };
-//         });
-//     });
-//     const restartBtn = document.getElementById('restart-btn');
-//     restartBtn.addEventListener('click', () => {
-//         startGame.style.display = 'none';
-//         startGameBtn.style.display = 'block';
-//         gameBoardArray.forEach((item) => {
-//             item.textContent = '';
-//             item.classList.remove('clicked');
-//             location.reload();
-//         });
-//         gameWon = false;
-//     });
-// }
-// game();
-
-
-
-
-
-
-////////////////////////////////////////////////////////////////
-
-
-
-
-
 // Gameboard module
 const gameBoard = (() => {
+    // Get all the squares on the game board
     const squares = Array.from(document.querySelectorAll('#gameBoard .square'));
 
     // Private function to check for winning combinations
@@ -97,6 +16,7 @@ const gameBoard = (() => {
             [2, 4, 6],
         ];
 
+        // Check if any of the winning combinations match the player's moves
         return winningCombinations.some(combination => {
             return combination.every(index => {
                 return squares[index].textContent === player;
@@ -106,6 +26,7 @@ const gameBoard = (() => {
 
     // Private function to check for tie condition
     const _checkTieCondition = () => {
+        // Check if all squares have been clicked
         return squares.every(square => {
             return square.classList.contains('clicked');
         });
@@ -114,8 +35,10 @@ const gameBoard = (() => {
     // Public function to handle player move
     const handlePlayerMove = (player, square) => {
         if (!square.classList.contains('clicked')) {
+            // Update the square with the player's marker
             square.textContent = player;
             square.classList.add('clicked');
+            // Check if the player has won or if there is a tie
             if (_checkWinningCombination(player)) {
                 return 'win';
             } else if (_checkTieCondition()) {
@@ -128,12 +51,14 @@ const gameBoard = (() => {
 
     // Public function to reset game board
     const resetGameBoard = () => {
+        // Reset all squares to their initial state
         squares.forEach(square => {
             square.textContent = '';
             square.classList.remove('clicked');
         });
     };
 
+    // Return the public functions
     return {
         handlePlayerMove,
         resetGameBoard,
@@ -144,16 +69,18 @@ const gameBoard = (() => {
 const Player = (marker) => {
     let name = '';
     return {
-      getName: () => name,
-      setName: (newName) => {
-        name = newName;
-      },
-      marker,
+        // Get the player's name
+        getName: () => name,
+        // Set the player's name
+        setName: (newName) => {
+            name = newName;
+        },
+        marker,
     };
-  };
-  
-  // Game module
-  const game = (() => {
+};
+
+// Game module
+const game = (() => {
     const startGame = document.getElementById('game');
     const startGameBtn = document.getElementById('start-game-btn');
     const winnerDisplay = document.getElementById('winner-display');
@@ -162,55 +89,66 @@ const Player = (marker) => {
     const player2 = Player('O');
     let currentPlayer = player1;
     let gameWon = false;
-  
+
     // Private function to switch players
     const _switchPlayers = () => {
-      currentPlayer = currentPlayer === player1 ? player2 : player1;
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
     };
-  
+
     // Private function to handle game over
     const _handleGameOver = (result) => {
-      if (result === 'win') {
-        winnerDisplay.textContent = `${currentPlayer.getName()} wins! Restart the game to play again.`;
-      } else if (result === 'tie') {
-        winnerDisplay.textContent = `Tie game! Restart the game.`;
-      }
-      gameWon = true;
+        // Get the winner's name
+        const winner = currentPlayer.getName();
+        if (result === 'win') {
+            // Display the winner's name
+            winnerDisplay.textContent = `${winner} wins! Restart the game to play again.`;
+        } else if (result === 'tie') {
+            winnerDisplay.textContent = `Tie game! Restart the game.`;
+        }
+        gameWon = true;
     };
-  
+
     // Event listener for start game button
     startGameBtn.addEventListener('click', () => {
-      const player1NameInput = prompt(`Please enter your name for X`);
-      const player2NameInput = prompt(`Please enter your name for O`);
-      player1.setName(player1NameInput.value || `Player ${player1.marker}`);
-      player2.setName(player2NameInput.value || `Player ${player2.marker}`);
-      startGame.style.display = 'block';
-      startGameBtn.style.display = 'none';
+        // Prompt players for their names
+        const player1NameInput = prompt(`Please enter your name for X`);
+        const player2NameInput = prompt(`Please enter your name for O`);
+        // Set the players' names
+        player1.setName(player1NameInput || `Player ${player1.marker}`);
+        player2.setName(player2NameInput || `Player ${player2.marker}`);
+        // Show the game board and hide the start button
+        startGame.style.display = 'block';
+        startGameBtn.style.display = 'none';
     });
-  
+
     // Event listener for game board squares
     gameBoard.resetGameBoard();
     document.querySelectorAll('#gameBoard .square').forEach(square => {
-      square.addEventListener('click', () => {
-        if (!gameWon) {
-          const result = gameBoard.handlePlayerMove(currentPlayer.marker, square);
-          if (result === 'win' || result === 'tie') {
-            _handleGameOver(result);
-          } else {
-            _switchPlayers();
-          }
-        }
-      });
+        square.addEventListener('click', () => {
+            if (!gameWon) {
+                // Handle the player's move
+                const result = gameBoard.handlePlayerMove(currentPlayer.marker, square);
+                if (result === 'win' || result === 'tie') {
+                    // Handle game over
+                    _handleGameOver(result);
+                } else {
+                    // Switch players
+                    _switchPlayers();
+                }
+            }
+        });
     });
-  
+
     // Event listener for restart button
     restartBtn.addEventListener('click', () => {
-      startGame.style.display = 'none';
-      startGameBtn.style.display = 'block';
-      winnerDisplay.textContent = '';
-      gameBoard.resetGameBoard();
-      currentPlayer = player1;
-      gameWon = false;
+        // Hide the game board and show the start button
+        startGame.style.display = 'none';
+        startGameBtn.style.display = 'block';
+        // Reset the game board and game state
+        winnerDisplay.textContent = '';
+        gameBoard.resetGameBoard();
+        currentPlayer = player1;
+        gameWon = false;
+        location.reload();
     });
-  })();
-  
+})();
